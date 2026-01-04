124678b146c04dcbda51d888ad93caf997e239ab
ManlhamTech <mts.manlham@gmail.com>
2026-01-04 12:00:58 +0200

    Update all changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google' // Import this
 import App from './App.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Wrap your app and add your Client ID */}
-    <GoogleOAuthProvider clientId="15228028717-idpobi15jqact7hudd0f8behb4jfa9r9.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>,
 )
